b18fa22839a9be6e67956fd1aefb6d773683c8fe
johneliud <johneliud4@gmail.com>
2025-02-13 12:30:02 +0300

    add: createMaze function to clear any previous maze and creating a new every time the game starts.

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
 const maze = [
-        "WWWWWWWWWWWWWWWWWWWWWWW",
-        "WPPPWPPPPPPPPPPPPPWPPPW",
-        "WPWPWPWWWWWPWWWWWPWPWPW", 
-        "WPPPPPPPWPPPPPWPPPPPPPW", 
-        "WWWPWPWPWPWWWPWPWPWPWWW", 
-        "WPPPWPWPWPW WPWPWPWPPPW", 
-        "WPWPWPWPWPWWWPWPWPWPWPW", 
-        "WPPPWPWPPPPPPPPPWPWPPPW", 
-        "WWWWWPWPWWWWWWWPWPWWWWW", 
-        "WPPPPPWPPPPPPPPPWPPPPPW", 
-        "WPWWWWWPGGGTGGGPWWWWWPW", 
-        "OPPPPPPPG     GPPPPPPPO", 
-        "WPWWWWWPGGGGGGGPWWWWWPW", 
-        "WPPPPPWPPPPPPPPPWPPPPPW", 
-        "WWWWWPWPWWWWWWWPWPWWWWW",
-        "WPPPWPWPPPPPPPPPWPWPPPW", 
-        "WPWPWPWPWPWWWPWPWPWPWPW",
-        "WPPPWPWPWPW WPWPWPWPPPW", 
-        "WWWPWPWPWPWWWPWPWPWPWWW", 
-        "WPPPPPPPWPPPPPWPPPPPPPW", 
-        "WPWPWPWWWWWPWWWWWPWPWPW", 
-        "WPPPWPPPPPPPPPPPPPWPPPW", 
-        "WWWWWWWWWWWWWWWWWWWWWWW",
-      ];
+  'WWWWWWWWWWWWWWWWWWWWWWW',
+  'WPPPWPPPPPPPPPPPPPWPPPW',
+  'WPWPWPWWWWWPWWWWWPWPWPW',
+  'WPPPPPPPWPPPPPWPPPPPPPW',
+  'WWWPWPWPWPWWWPWPWPWPWWW',
+  'WPPPWPWPWPW WPWPWPWPPPW',
+  'WPWPWPWPWPWWWPWPWPWPWPW',
+  'WPPPWPWPPPPPPPPPWPWPPPW',
+  'WWWWWPWPWWWWWWWPWPWWWWW',
+  'WPPPPPWPPPPPPPPPWPPPPPW',
+  'WPWWWWWPGGGTGGGPWWWWWPW',
+  'OPPPPPPPG     GPPPPPPPO',
+  'WPWWWWWPGGGGGGGPWWWWWPW',
+  'WPPPPPWPPPPPPPPPWPPPPPW',
+  'WWWWWPWPWWWWWWWPWPWWWWW',
+  'WPPPWPWPPPPPPPPPWPWPPPW',
+  'WPWPWPWPWPWWWPWPWPWPWPW',
+  'WPPPWPWPWPW WPWPWPWPPPW',
+  'WWWPWPWPWPWWWPWPWPWPWWW',
+  'WPPPPPPPWPPPPPWPPPPPPPW',
+  'WPWPWPWWWWWPWWWWWPWPWPW',
+  'WPPPWPPPPPPPPPPPPPWPPPW',
+  'WWWWWWWWWWWWWWWWWWWWWWW',
+];
 
-      const tileClasses = {
-        W: "wall",
-        P: "path",
-        G: "ghost-pen",
-        T: "gate",
-        O: "warp"
-      };
+const tileClasses = {
+  W: 'wall',
+  P: 'path',
+  G: 'ghost-pen',
+  T: 'gate',
+  O: 'warp',
+};
 
-      const grid = document.querySelector(".grid");
-      maze.forEach(row => {
-        row.split("").forEach(cell => {
-          const div = document.createElement("div");
-          div.classList.add("tile", tileClasses[cell]);
-          grid.appendChild(div);
-        });
-      });
+const grid = document.querySelector('.grid');
+maze.forEach((row) => {
+  row.split('').forEach((cell) => {
+    const div = document.createElement('div');
+    div.classList.add('tile', tileClasses[cell]);
+    grid.appendChild(div);
+  });
+});
+
+function createMaze() {
+  const grid = document.querySelector('.grid');
+
+  // Clear previous grid before creating a new.
+  grid.innerHTML = '';
+  maze.forEach((row) => {
+    row.split('').forEach((cell) => {
+      const div = document.createElement('div');
+      div.classList.add('tile', tileClasses[cell]);
+      grid.appendChild(div);
+    });
+  });
+}
