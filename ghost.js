544a5bc6224f79b981f736ed407eb351319301da
John <johneliud4@gmail.com>
2025-02-13 21:07:45 +0300

    add: Ghost constructor defining properties of ghost elements.

---
+++ ghost.js
@@ -0,0 +1,30 @@
+import { getTile } from './utils.js';
+
+export class Ghost {
+  constructor(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+
+    // Create ghost element
+    this.element = document.createElement('div');
+    this.element.classList.add('ghost', this.color);
+
+    // Append ghost to correct tile
+    const tile = getTile(this.x, this.y);
+    if (tile) {
+      tile.appendChild(this.element);
+    }
+  }
+}
+
+// Create ghosts and their starting postitons
+export function createGhosts() {
+  const ghosts = [
+    new Ghost(10, 11, 'red'), // Blinky
+    new Ghost(11, 11, 'pink'), // Pinky
+    new Ghost(12, 11, 'blue'), // Inky
+    new Ghost(13, 11, 'orange'), // Clyde
+  ];
+  return ghosts;
+}
