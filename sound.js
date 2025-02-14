28a75da0a0f9bc0c3bf067a55a0abf97c2074c61
johneliud <johneliud4@gmail.com>
2025-02-14 11:04:03 +0300

    refactor: loop over each sound for continuos play.

--- sound.js
+++ sound.js
@@ -5,3 +5,9 @@ export const deathSound = new Audio('sounds/pacman_death.wav');
 export const eatingGhostSound = new Audio('sounds/pacman_eatghost.wav');
 export const extraManSound = new Audio('sounds/pacman_extrapac.wav');
 export const intermissionSound = new Audio('sounds/pacman_intermission.wav');
+
+// Preload sounds for continuos play.
+const sounds = [introSound, pacmanMunchSound, eatingFruitSound, deathSound, eatingGhostSound, extraManSound, intermissionSound];
+sounds.forEach(sound => {
+  sound.preload = 'auto';
+});
