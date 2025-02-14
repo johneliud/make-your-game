c1411536f1be8d154dd49d05a7084ea69de3cd7c
johneliud <johneliud4@gmail.com>
2025-02-14 10:28:22 +0300

    add: sound effects variables to be used during different stages in the game.

---
+++ sound.js
@@ -0,0 +1,7 @@
+export const introSound = new Audio('sounds/pacman_beginning.wav');
+export const pacmanMunchSound = new Audio('sounds/pacman_chomp.wav');
+export const eatingFruitSound = new Audio('sounds/pacman_eatfruit.wav');
+export const deathSound = new Audio('sounds/pacman_death.wav');
+export const eatingGhostSound = new Audio('sounds/pacman_eatghost.wav');
+export const extraManSound = new Audio('sounds/pacman_extrapac.wav');
+export const intermissionSound = new Audio('sounds/pacman_intermission.wav');
