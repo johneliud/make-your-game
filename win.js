07e43cef1c8a7cc813b6fbdaff4a0a3751f67199
JoeOwino <Joeoowino@gmail.com>
2025-02-14 15:40:59 +0300

    refactor: update win condition check to use Maze class

--- win.js
+++ win.js
@@ -1,8 +1,9 @@
 import { intermissionSound } from './sound.js';
+import { Maze } from './script.js';
 
-export function checkWinCondition(maze) {
+export function checkWinCondition() {
   // Check if all pellets and power pellets are collected
-  for (let row of maze) {
+  for (let row of Maze) {
     if (row.includes('P')) {
       return false; // Pellets still exist
     }
